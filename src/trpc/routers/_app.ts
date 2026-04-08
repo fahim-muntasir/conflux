9cb4d0278f76deaec6abbd3e7fc51be5843e1951
Fahim muntasir <fahimmuntasir0909@gmail.com>
2026-04-08 20:32:23 +0600

    backend-infrastructure

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,12 @@
+// import { z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+ 
+export const appRouter = createTRPCRouter({
+  health: baseProcedure.query(async () => {
+    // throw new Error('Health check failed');
+    return { status: 'ok' };
+  }),
+});
+ 
+// export type definition of API
+export type AppRouter = typeof appRouter;
